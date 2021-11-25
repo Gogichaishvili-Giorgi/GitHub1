bbf8476a4ed35e704836970bca50fc4ad85b1e4d
Gogicha <gogichaishvilig544@gmail.com>
2021-11-25 13:21:57 +0400

    New Models file added

---
+++ GitHub1/Models.swift
@@ -0,0 +1,12 @@
+//
+//  Models.swift
+//  GitHub1
+//
+//  Created by Giorgi Gogichaishvili on 11/25/21.
+//
+
+import Foundation
+struct Family {
+    var name: String
+    var lastName: String
+}
