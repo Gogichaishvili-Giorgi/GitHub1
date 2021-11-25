be25dd52b13125d1e256cfd169357ac8b9c137c2
Gogicha <gogichaishvilig544@gmail.com>
2021-11-25 13:47:26 +0400

    New file about informations

---
+++ GitHub1/Informationsaboutgit.swift
@@ -0,0 +1,21 @@
+//
+//  Informationsaboutgit.swift
+//  GitHub1
+//
+//  Created by Giorgi Gogichaishvili on 11/25/21.
+//
+
+import Foundation
+//stapps
+/*
+ 1. in Github create repository
+ 2. in terminal point out on exist file like this : cd Desktop - XCode stuffs - GitHub-GitHub1
+ 3. git init
+ 4. git remote add origin here pastdownload code
+ 5. git status
+ 6. git pull origin master
+ 7. git add .
+ 8. git commit -m "bla bla bla"
+ 9. git push origin master
+ if username and password is required - username - "Gogicha" and password is Personal access token if you don't rememeber that token it is recommended to create new token and copy and paste it somewhere else
+ */
